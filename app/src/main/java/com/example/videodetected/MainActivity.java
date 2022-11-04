e5b84b943edcc5cf3853ab52ff6425d10f752fe7
jimieguang <1137040634@qq.com>
2022-11-04 09:58:46 +0800

    基本样式构建与sidesbar

--- app/src/main/java/com/example/videodetected/MainActivity.java
+++ app/src/main/java/com/example/videodetected/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.videodetected;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 去除默认标题栏
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        // 状态栏改为全透明
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(Color.TRANSPARENT);
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        // 进入主页
         setContentView(R.layout.activity_main);
-    }
+        final ImageView menu_main = findViewById(R.id.menu_main);
+
+        // 点击按钮打开侧边栏
+        menu_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DrawerLayout drawer = findViewById(R.id.drawer_layout);
+                drawer.openDrawer(GravityCompat.START);
+            }
+        });
+
+}
 }
